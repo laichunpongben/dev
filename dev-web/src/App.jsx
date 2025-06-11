a3d7ffc4736df09b0e8cc1ef899a9987320d4bfe
Ben Lai <laichunpongben@gmail.com>
2025-06-12 01:09:51 +0800

    reduce mobile font sizes

--- dev-web/src/App.jsx
+++ dev-web/src/App.jsx
@@ -321,6 +321,7 @@ function App() {
             mt: { xs: 1, sm: 2 },
             mb: { xs: 1, sm: 2 },
             textAlign: 'left',
+            fontSize: { xs: '0.6rem', sm: '1rem' },
           }}
         >
           <DecryptedText text="Who Am I? 
@@ -411,6 +412,7 @@ function App() {
           textAlign: 'center',
           my: { xs: 1, sm: 2 },
           pb: { xs: 1, sm: 0 },
+          fontSize: { xs: '0.6rem', sm: '0.875rem' },
         }}
       >
         © 2025 Databookman by Ben Lai
