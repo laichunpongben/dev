fc054b980c82a94187f842e6ece18c8ca5a617fc
Ben Lai <laichunpongben@gmail.com>
2025-06-12 00:03:04 +0800

    Add onClick prop to ShinyText

--- dev-web/src/components/ShinyText.jsx
+++ dev-web/src/components/ShinyText.jsx
@@ -1,12 +1,19 @@
 import './ShinyText.css'
 
-const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+const ShinyText = ({
+  text,
+  disabled = false,
+  speed = 5,
+  className = '',
+  onClick,
+}) => {
   const animationDuration = `${speed}s`
 
   return (
     <div
       className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`}
       style={{ animationDuration }}
+      onClick={onClick}
     >
       {text}
     </div>
