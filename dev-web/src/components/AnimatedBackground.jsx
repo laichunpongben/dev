ec8df2733edf897fd1931894997051a32f9a37c3
Ben Lai <laichunpongben@gmail.com>
2025-06-12 00:06:54 +0800

    Use bunny animation SVG

--- dev-web/src/components/AnimatedBackground.jsx
+++ dev-web/src/components/AnimatedBackground.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './AnimatedBackground.css'
-import bunny from '../assets/images/usagi_running_colored.png'
+import bunny from '../assets/images/usagi_running_colored.svg'
 
 export default function AnimatedBackground({ className = '' }) {
   return (
