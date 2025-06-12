9b5f2200b88fb2401654fdac82dc8c6f0de87d7b
Ben Lai <laichunpongben@gmail.com>
2025-06-12 10:50:56 +0800

    use send svg in pwa manifest

--- dev-web/vite.config.js
+++ dev-web/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['send.svg'],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
+      },
+      manifest: {
+        name: 'Dev Portal',
+        short_name: 'DevPortal',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'send.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
 })
