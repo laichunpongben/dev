2e13c064f99d5998fe05f0a32d87843c62d43b14
Ben Lai <laichunpongben@gmail.com>
2025-06-16 14:05:27 +0800

    Rename warmup scripts

--- dev-web/schedule-warmup.sh
+++ dev-web/schedule-warmup.sh
@@ -3,8 +3,8 @@ set -e
 
 mkdir -p /etc/crontabs
 # run once at start
-/warmup-cron.sh
+/ping-endpoints.sh
 # schedule every minute
-echo "* * * * * /warmup-cron.sh" > /etc/crontabs/root
+echo "* * * * * /ping-endpoints.sh" > /etc/crontabs/root
 crond
 exec nginx -g 'daemon off;'
